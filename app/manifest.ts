8f23d3fa22ffbd2502b9f48c4615e6be39aac519
KimKeeHyun <kimkee7@gmail.com>
2024-12-01 20:40:39 +0900

    setting, login

---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'NEXTIO',
+    short_name: 'NEXTIO',
+    description: 'NEXTIO = Supabase + Nest.js',
+    start_url: `${process.env.SITE_URL}`,
+    display: 'standalone',
+    background_color: '#1a1a1a',
+    theme_color: '#1a1a1a',
+    icons: [
+        {
+        src: `${process.env.SITE_URL}img/favicon.ico`,
+        sizes: '48x48',
+        type: 'image/x-icon'
+      },
+      {
+        src: `${process.env.SITE_URL}img/icon_app.png`,
+        type: "image/png",
+        sizes: "512x512"
+      }
+    ],
+  }
+}
