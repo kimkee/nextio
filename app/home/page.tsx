2990f80eebcea7af415e5c0727a5dfe3adcbbfbb
KimKeeHyun <kimkee7@gmail.com>
2024-12-02 16:05:12 +0900

    home4

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,16 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react';
-import '@/app/fontawesome';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export const runtime = 'edge';
 export default function Home() {
-
-
-
   return (
-    
       <div className="container page home">
         <main className="contents">
-          <h2><FontAwesomeIcon icon="house" /> Home </h2>
+          <h2>Home </h2>
         </main>
       </div>
     
