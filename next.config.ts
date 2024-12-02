4214e721da1b6e3dd9118752e7c7073ff8414280
KimKeeHyun <kimkee7@gmail.com>
2024-12-02 15:38:22 +0900

    redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true, // 영구 리다이렉션 (301), 임시 리다이렉션으로 하려면 false로 설정 (307)
+      },
+    ];
+  },
+  // 추가적인 설정 옵션을 여기에 추가할 수 있습니다
 };
 
 export default nextConfig;
