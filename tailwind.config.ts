6cd617086437276138b1902e48852f14ddc991e6
KimKeeHyun <kimkee7@gmail.com>
2024-12-05 15:58:23 +0900

    Revert "Revert "회원""
    This reverts commit 064f294436936b652efcf58404793ec653d5c9aa.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,17 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'primary': '#52a8ff',
+        'secondary': '#facc15',
       },
+      fontSize: {
+        '9': ['0.5625rem', '1.4'], // 10px
+        '10': ['0.625rem', '1.4'], // 10px
+        'xt': ['0.6875rem', '1.4'], // 11px
+        'ss': ['0.8125rem', '1.4'], // 13px
+        'md': ['0.9375rem', '1.4'], // 15px
+        'xx': ['1.375rem', '1.4'], // 22px
+      }
     },
   },
   plugins: [],
