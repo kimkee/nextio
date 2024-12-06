d355dfc1682c464929b8ddb382714afe0523dd34
KimKeeHyun <kimkee7@gmail.com>
2024-12-06 16:35:34 +0900

    모달0

--- app/test/photos/[id]/page.tsx
+++ app/test/photos/[id]/page.tsx
@@ -1,9 +1,6 @@
 export const dynamicParams = false;
 
-export function generateStaticParams() {
-  let slugs = ["1", "2", "3", "4", "5", "6"];
-  return slugs.map((slug) => ({ id: slug }));
-}
+
 
 export default async function PhotoPage({
   params,
