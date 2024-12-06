925786274fee949e7c2cca4bc973cee17ed570bf
KimKeeHyun <kimkee7@gmail.com>
2024-12-06 22:42:51 +0900

    .

--- app/test/[opts]/[cate]/[id]/page.tsx
+++ app/test/[opts]/[cate]/[id]/page.tsx
@@ -9,5 +9,14 @@ export default async function PhotoPage({
   const id = (await params).id;
   const cate = (await params).cate;
   const opts = (await params).opts;
-  return <div className="contents">/test/{opts}/{cate}/{id}</div>;
+  return (
+    <main className="contents">
+        <h2>/test/{opts}/{cate}/{id}</h2>
+        <div className="flex flex-col">
+            <p>opts : {opts}</p>
+            <p>cate : {cate}</p>
+            <p>id : {id}</p>
+        </div>
+    </main>
+  );
 }
