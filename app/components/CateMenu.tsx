e05ee2aacb4a6ade32a69a5da071400d90808462
KimKeeHyun <kimkee7@gmail.com>
2024-12-09 20:51:28 +0900

    CateMenu

--- app/components/CateMenu.tsx
+++ app/components/CateMenu.tsx
@@ -1,19 +1,19 @@
 
 'use client';
 import React, { useState, useEffect, useRef } from 'react';
-import { usePathname, useRouter, useParams  } from 'next/navigation';
+import { usePathname, useRouter, useParams } from 'next/navigation';
 import Link from 'next/link';
 import '../style/CateMenu.scss';
-export default function CateMenu({menu, opts}:{menu:any[], opts:string}) {
+export default function CateMenu({ menu, opts }: { menu: any[], opts: string }) {
   const params = useParams()
   const [slideActive, slideActiveSet] = useState(0);
   const cateBoxRef = useRef<HTMLDivElement>(null);
-  const activeBtnRef = useRef<HTMLAnchorElement>(null);
-
-  const goSlide = (num:number) => {
+  const activeBtnRef = useRef<HTMLButtonElement>(null);
+  const router = useRouter();
+  const goSlide = (num: number) => {
     const catebox = cateBoxRef.current;
     const btnAct = activeBtnRef.current;
-    if(catebox && btnAct){
+    if (catebox && btnAct) {
       const cateboxWid = catebox?.offsetWidth * 0.5;
       const btnActLeft = btnAct?.offsetLeft;
       const btnActWid = btnAct?.offsetWidth * 0.5;
@@ -28,35 +28,39 @@ export default function CateMenu({menu, opts}:{menu:any[], opts:string}) {
   useEffect(() => {
     goSlide(slideActive);
     // console.log(menu);
-    
+
     return () => { }
     // eslint-disable-next-line
   }, [cateID, slideActive, menu, opts]);
 
   return (
-  <>
-    <div className={"cate-box " + cateID}>
-      <div className="inr" ref={cateBoxRef}>
-        <ul className="list">
-          <li data-index="0" className={ "0" === cateID ? "active" : '' }>
-            <Link className="bt text-xs" href={`/list/${opts}/0`} ref={cateID === "0" ? activeBtnRef : null}>전체</Link>
-          </li>
-          { menu.map( (item: { id: string; name: string;  },idx:number) => { 
-            return (
-              <li data-index={idx+1} key={item.id} data-cate={item.id} className={ item.id.toString() === cateID ? "active" : '' }>
-                <Link 
-                  className="bt text-xs" 
-                  href={`/list/${opts}/${item.id}`}
-                  ref={item.id.toString() === cateID ? activeBtnRef : null}
-                > 
-                  { item.name } 
-                </Link>
+    <>
+      <div className={"cate-box " + cateID}>
+        <div className="inr" ref={cateBoxRef}>
+          {menu.length > 0 ?
+            <ul className="list">
+              <li data-index="0" className={"0" === cateID ? "active" : ''}>
+                <button className="bt text-xs" onClick={() => router.push(`/list/${opts}/0`)} ref={cateID === "0" ? activeBtnRef : null}>전체</button>
               </li>
-            )
-          })}
-        </ul>
+              {menu.map((item: { id: string; name: string; }, idx: number) => {
+                return (
+                  <li data-index={idx + 1} key={item.id} data-cate={item.id} className={item.id.toString() === cateID ? "active" : ''}>
+                    <button
+                      className="bt text-xs"
+                      onClick={() => router.push(`/list/${opts}/${item.id}`)}
+                      ref={item.id.toString() === cateID ? activeBtnRef : null}
+                    >
+                      {item.name}
+                    </button>
+                  </li>
+                )
+              })}
+            </ul>
+            :
+            null
+          }
+        </div>
       </div>
-    </div>
-  </>  
+    </>
   )
 }
