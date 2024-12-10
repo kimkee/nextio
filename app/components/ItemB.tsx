aefcbd6df6a265775ebb84b72c2aa9aedcfbcb1a
KimKeeHyun <kimkee7@gmail.com>
2024-12-10 18:04:23 +0900

    ui-star

--- app/components/ItemB.tsx
+++ app/components/ItemB.tsx
@@ -1,10 +1,11 @@
+'use client';
 import React from 'react';
 import Link from 'next/link';  // useParams , Outlet, useSearchParams, useLocation
 import './ItemB.css';
-// import StarPoint from '../components/StarPoint';
+import StarPoint from '@/app/components/StarPoint';
 
 // Movie, TV 리스트 유닛
-export default function ItemB({data, opts, cate}) {
+export default function ItemB({data, opts, cate}: any) {
   // console.log(data);
   
   const img = `//image.tmdb.org/t/p/w200/${data.poster_path}`;
@@ -18,7 +19,7 @@ export default function ItemB({data, opts, cate}) {
           <img src={`${img}`} alt={tit} className='img' /* onError={ui.error.poster} *//>
         </div>
         <div className="desc">
-          {/* <StarPoint point={data.vote_average} /> */}
+          <StarPoint point={data.vote_average} />
         </div>
       </div>
       <div className="mt-1 text-xs line-clamp-1 overflow-hidden">{tit}</div>
