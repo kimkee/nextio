a31dcce7c5d912f13c12af67faf36cafee8a3864
KimKeeHyun <kimkee7@gmail.com>
2024-12-13 13:00:47 +0900

    404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,20 +1,33 @@
 // app/not-found.tsx
+'use client';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function NotFound() {
+  const router = useRouter();
   return (
     <div className='container items-center justify-center'>
       <main className='flex flex-col gap-4 row-start-2 items-center'>
-        <h1 className='font-bold text-center flex flex-col gap-2'>
-          <span className='text-4xl'>404</span>
-          <span className='text-xl'>Page Not Found</span>
-        </h1>
-        <p className='mt-4 text-sm'>
-          Sorry, the page you are looking for does not exist.
-        </p>
-        <Link href='/' className='btn mt-6 text-blue-500'>
-          Go back to Home
-        </Link>
+        <div className="">
+          <h1 className='font-bold text-center flex flex-col gap-2'>
+            <span className='text-4xl'>404</span>
+            <span className='text-xl'>Page Not Found</span>
+          </h1>
+          {/* <p className='mt-4 text-sm text-white/70 leading-5 text-center'>
+            Sorry!!
+          </p> */}
+          <div className="flex gap-2 mt-8 justify-center">
+              <button type="button" className="btn btn-sm !px-3" onClick={ ()=>{router.back()} }>
+                <FontAwesomeIcon icon={['fas', 'arrow-left']} />
+                <em>Back</em>
+              </button>
+              <button type="button" className="btn btn-sm !px-3" onClick={ ()=>{router.push(`/`)} }>
+                <FontAwesomeIcon icon={['fas', 'home']} />
+                <em>Home</em>
+              </button>
+          </div>
+        </div>
       </main>
     </div>
   );
