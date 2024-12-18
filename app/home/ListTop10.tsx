0c740f271843e1e5d138ce99249c929ff7bc7d8f
KimKeeHyun <kimkee7@gmail.com>
2024-12-18 15:13:47 +0900

    home listtop10

--- app/home/ListTop10.tsx
+++ app/home/ListTop10.tsx
@@ -72,13 +72,12 @@ export default  function ListSet({opts}:{opts:{media:string, list:string, cate:s
   return(
     <>
       
-      <section className="sect mnList mb-3">
+      <section className="sect mnList mb-4">
 
-        <div className="hbox flex justify-between items-center min-h-8 mb-1.5 leading-none px-5">
-          <Link className="link inline-flex items-center" href={`/list/${opts.media}/${opts.cate || 0}`}>
-            <h4 className="tts text-sm">{opts.title}</h4>
-            <FontAwesomeIcon icon={['fas', 'chevron-right']} className='w-3 h-3 ml-1'/>
-          </Link>
+        <div className="hbox flex justify-between items-center min-h-8 mb-1.5 leading-none px-5 py-2">
+          <div className="link inline-flex items-center italic">
+            <h4 className="tts text-xl">{opts.title}</h4>
+          </div>
           <div className={`bt-nav ${isNav ? '' : 'hidden'}`}>
             <button type="button" disabled={isNavPrev} onClick={(e)=>goScroll('prev', e)} className="bt w-4 h-4 inline-flex items-center justify-center -mx-0.5 text-white/30 hover:text-primary"><FontAwesomeIcon icon={['fas', 'caret-left' ]} className='w-3 h-3' /></button>
             <button type="button" disabled={isNavNext} onClick={(e)=>goScroll('next', e)} className="bt w-4 h-4 inline-flex items-center justify-center -mx-0.5 text-white/30 hover:text-primary"><FontAwesomeIcon icon={['fas', 'caret-right']} className='w-3 h-3' /></button>
@@ -86,7 +85,7 @@ export default  function ListSet({opts}:{opts:{media:string, list:string, cate:s
         </div>
 
         <div ref={scrollBoxRef}
-          className="inr flex flex-nowrap overflow-y-hidden overflow-x-auto px-3 scrollbar-hidden scroll-smooth"
+          className="inr flex flex-nowrap overflow-y-hidden overflow-x-auto px-1 scrollbar-hidden scroll-smooth"
           onMouseEnter={ ()=>scrollBoxRef.current?.addEventListener('wheel', handleWheel) }
           onMouseLeave={ ()=>scrollBoxRef.current?.removeEventListener('wheel', handleWheel) }
           onScroll={ handeScroll }
@@ -96,11 +95,13 @@ export default  function ListSet({opts}:{opts:{media:string, list:string, cate:s
               const img = `https://image.tmdb.org/t/p/w154${data.poster_path}` ;
               const tit = data.title || data.name;
               return (
-                <div key={idx} className="pbox box block w-[calc(24%-3%)] min-w-[calc(24%-3%)] mx-[0.4rem]  break-all">
-                  <Link className="box block relative rounded-sm overflow-hidden w-full bg-black pb-[calc(450%/300*100)] mb-1"
+                <div key={idx} className="pbox box pt-2 pl-2 block w-[calc(30%-0.625rem)] min-w-[calc(30%-0.625rem)] mx-[0.4rem]  break-all last:mr-3">
+                  <Link className="box block relative rounded-sm w-full bg-black pb-[calc(450%/300*100)] mb-1"
                      href={`/list/${opts.media}/${opts.cate}/${data.id}`}
                   >
                     <Img width={300} height={450} src={`${img}`} alt={tit} srcerr='/img/common/non_poster.png' unoptimized={true} className='img img absolute object-cover w-full h-full'/>
+                    <div className='absolute -top-3 -left-2 text-white text-3xl font-extrabold drop-shadow-[0_1px_3px_rgba(0,0,0,0.5)] italic z-10'>{idx + 1}</div>
+                    <div className="info absolute left-0 bottom-0 right-0 bg-gradient-to-t from-black/62 to-transparent p-1"><StarPoint point={data.vote_average} opts={{cls:'lg'}} /></div>
                   </Link>
                 </div>
               )
