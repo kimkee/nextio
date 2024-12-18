88996a44c83b01d81d5bb4307f0bbdfd8ca58f3b
KimKeeHyun <kimkee7@gmail.com>
2024-12-18 18:04:32 +0900

    Modal

--- app/search/[opts]/page.tsx
+++ app/search/[opts]/page.tsx
@@ -1,26 +1,36 @@
-'use client';
-import Image from "next/image";
-import React, { useState, useEffect, useRef } from 'react';
-import { usePathname, useRouter, useParams  } from 'next/navigation';
-import { supabase } from '@/app/supabase'; 
-import { Provider } from '@supabase/supabase-js';
-import '@/app/lib/fontawesome';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import Link from 'next/link';
 export const runtime = 'edge';
-export default function Search() {
+export const dynamicParams = false;
 
-  const params = useParams();
-  const opts = params.opts;
-  const cateID = params.cate;
-  // cateID === undefined && navigate(`/${opts}/0`) ;
-
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ cate: string, opts: string, id: string }>;
+}) {
+  
+  const cate = (await params).cate;
+  const opts = (await params).opts;
   return (
-    
-      <div className="container page movie list">
-        <main className="contents">
-          <h2><FontAwesomeIcon icon="house" /> Search / {opts} </h2>
-        </main>
+    <main className="p-6">
+
+      <div className='flex gap-4'>
+        <Link className="btn" href={`/search/movie/`}>MOVIE</Link>
+        <Link className="btn" href={`/search/tv/`}>TV</Link>
       </div>
-    
+
+      <p>{`/search/${opts}`}</p>
+      <ul className="grid grid-cols-2 gap-4 mt-4">
+        {[1,2,3,4,5,6,7,8,9,10,11,12].map((idx) => (
+          <li key={idx}>
+            <Link
+              className="border border-white/20 rounded-md p-4 h-40 flex flex-col gap-1 justify-center items-center text-md uppercase"
+              href={`/search/${opts}/${idx}`} passHref>
+              {opts} - {idx}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
