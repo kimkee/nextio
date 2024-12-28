b2387b4b6dd810f57294ee8b003a8b5b83cc2bec
KimKeeHyun <kimkee7@gmail.com>
2024-12-28 17:34:46 +0900

    리뷰입력창

--- app/list/DetailRev.tsx
+++ app/list/DetailRev.tsx
@@ -233,7 +233,8 @@ console.log(myinfo);
               placeholder={`${myinfo?.id ? `감상평을 남겨보세요. (최대${revNumMax}자)`:`로그인 후 감상평을 남겨보세요.`}`}
             ></textarea>
             <div className="bts mt-2">
-              <button type="button" className="btn sm btsend !text-12" disabled={ revNumNow < 1 } onClick={sendReview}>
+              <button type="button" className="btn btn-sm btsend !text-12 w-full bg-[rgb(0_0_0_/_38%)] border-white/10 disabled:bg-[rgb(0_0_0_/_20%)] rounded-md" 
+                disabled={ revNumNow < 1 } onClick={sendReview}>
                 <FontAwesomeIcon icon={['fas', 'paper-plane']} /> <em>등록</em>
               </button>
             </div>
