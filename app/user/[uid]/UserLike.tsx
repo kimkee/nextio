ca4750da5dc8615add8ac1d8a2ad5681170c533a
KimKeeHyun <kimkee7@gmail.com>
2025-01-19 14:27:49 +0900

    ...

--- app/user/[uid]/UserLike.tsx
+++ app/user/[uid]/UserLike.tsx
@@ -197,7 +197,7 @@ export default function UserLike({uInfo,user,swiper1dep}:{uInfo:any,user:any,swi
             </div>
             }
           </>
-          }      
+          }
       </>
     )
   }
