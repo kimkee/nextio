83dc6de3869ac41258b3932aeadd091e03da6556
KimKeeHyun <kimkee7@gmail.com>
2026-04-17 14:33:44 +0900

    NotFoundContent

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,8 @@
 // app/not-found.tsx
-'use client';
-
-import { useRouter } from 'next/navigation';
 import { Suspense } from 'react';
 import NotFoundContent from './components/NotFoundContent';
 
 export default function NotFound() {
-  const router = useRouter();
   return (
     <Suspense fallback={null}>
       <NotFoundContent />
